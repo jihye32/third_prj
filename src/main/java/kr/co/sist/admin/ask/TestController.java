c2f8d6b84b8b102ce601040f53b21c4a0253e9f1
gkwlgp <thoth_0302@naver.com>
2026-01-14 13:51:58 +0900

    사용자 sample desing 완성 및 package, folder 생성

---
+++ src/main/java/kr/co/sist/admin/ask/TestController.java
@@ -0,0 +1,23 @@
+package kr.co.sist.admin.ask;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class TestController {
+	
+	@RequestMapping(value = "/", method = {RequestMethod.GET, RequestMethod.POST})
+	public String testIndex() {
+		return "index";
+	}
+	
+	@RequestMapping(value = "/header", method = {RequestMethod.GET, RequestMethod.POST})
+	public String testHeader() {
+		return "template/header";
+	}
+	@RequestMapping(value = "/footer", method = {RequestMethod.GET, RequestMethod.POST})
+	public String testFooter() {
+		return "template/footer";
+	}
+}
