6d55a8e082d24b75d375a51e030a1e1559555ed8
gkwlgp <thoth_0302@naver.com>
2026-01-16 15:02:30 +0900

    user sample

--- src/main/java/kr/co/sist/user/productdetail/ProductDetailController.java
+++ src/main/java/kr/co/sist/user/productdetail/ProductDetailController.java
@@ -1,13 +1,28 @@
 package kr.co.sist.user.productdetail;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProductDetailController {
 
-	@GetMapping("product/detail")
+	@GetMapping("/product/detail")
 	public String testPage() {
-		return "product_detail/sample";
+		return "product_detail/product_detail";
+	}
+	@GetMapping("/chat/list")
+	public String testChat() {
+		return "template/wrapper_frm :: chatListForm";
+	}
+	@GetMapping("/report/form")
+	public String testReport(Model model) {
+		
+		
+		//service, DAO 작업하는 곳
+		
+		
+		//iframe 디자인을 작업한 폴더명/iframe 디자인을 작업한 html명 :: 불러올 fragment명
+		return "pruduct_detail/frm :: deliveryForm";
 	}
 }
---
+++ src/main/java/kr/co/sist/user/sample/SampleController.java
@@ -0,0 +1,18 @@
+package kr.co.sist.user.sample;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SampleController {
+
+	@GetMapping("/test/main")
+	public String sampleMain() {
+		return "/sample/main_sample";
+	}
+	
+	@GetMapping("/test")
+	public String sampleTest() {
+		return "sample/wrapper_sample :: fragmentSample";
+	}
+}
