63575f2ac0f43494f4356bc23c564f47ef9bf9c1
lsw1004 <sw100411hw@gmail.com>
2026-02-06 17:16:18 +0900

    누락된 sellerPage Mapper 추가

--- src/main/java/kr/co/sist/user/sellerpage/SellerPageController.java
+++ src/main/java/kr/co/sist/user/sellerpage/SellerPageController.java
@@ -1,12 +1,23 @@
 package kr.co.sist.user.sellerpage;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/store")
 @Controller
 public class SellerPageController {
-	@GetMapping("/sellerPage")
-	public String sellerPage() {
+	
+	@Autowired
+	private SellerPageService sps;
+	
+	@GetMapping("/{storeNum}")
+	public String sellerPage(@PathVariable int storeNum, Model model) {
+		System.out.println(sps.searchSeller(storeNum));
+		model.addAttribute("temp",sps.searchSeller(storeNum));
 		return "/seller_page/sellerPage";
 	}
 }// class
--- src/main/java/kr/co/sist/user/sellerpage/SellerPageService.java
+++ src/main/java/kr/co/sist/user/sellerpage/SellerPageService.java
@@ -1,8 +1,24 @@
 package kr.co.sist.user.sellerpage;
 
+import org.apache.ibatis.exceptions.PersistenceException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SellerPageService {
 
-}
+	@Autowired(required = false)
+	private SellerPageDAO spDAO;
+	
+	public SellerPageDomain searchSeller(int storeNum) {
+		SellerPageDomain spd = null;
+		try {
+			spd = spDAO.selectSeller(storeNum);
+		} catch (PersistenceException pe) {
+			pe.printStackTrace();
+		}// end cathc
+		return spd;
+	}// searchSeller
+	
+	
+}// class
