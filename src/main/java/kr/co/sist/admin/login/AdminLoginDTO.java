d4800b0ade35dd17dc096336c91ef75422ad4cf9
daminlim1119 <daminlim1119@gmail.com>
2026-02-06 18:56:43 +0900

    0206 관리자

--- src/main/java/kr/co/sist/admin/login/AdminLoginDTO.java
+++ src/main/java/kr/co/sist/admin/login/AdminLoginDTO.java
@@ -1,12 +1,16 @@
 package kr.co.sist.admin.login;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Setter
 @Getter
 @ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class AdminLoginDTO {
 	
 	private String adminId;
--- src/main/java/kr/co/sist/admin/sales/SalesController.java
+++ src/main/java/kr/co/sist/admin/sales/SalesController.java
@@ -4,16 +4,41 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SalesController {
+	@Autowired
+SalesService ss;
 	
-
     @GetMapping("/manage/sales") // 브라우저에 /manage/dashboard을 입력하면 실행
-    public String dashboardPage(Model model) {
+    public String salesPage(@RequestParam(value = "safepayMonth", required = false) String safepayMonth,Model model) {
+    	
+    	if (safepayMonth == null || safepayMonth.isEmpty()) {
+            safepayMonth = LocalDate.now().format(DateTimeFormatter.ofPattern("YYYY-MM"));
+        }
+        
+        int monthNow = ss.selectSafepayMonthNow(safepayMonth);
+        int monthLast = ss.selectSafepayLastMonth(safepayMonth);
+        model.addAttribute("monthNow", monthNow);
+        model.addAttribute("monthLast", monthLast);
+        model.addAttribute("currentMonth", safepayMonth); // input에 다시 뿌려줄 값
+    	
+    	
+    	
+    	model.addAttribute("safepayToday",ss.selectSafepayToday());
+    	model.addAttribute("safepayYesterday",ss.selectSafepayYesterday());
+    	model.addAttribute("safepayMonth",ss.selectSafepayMonth());
+    	
+    	model.addAttribute("chargeNow",ss.selectCharge());
+    	
+    	model.addAttribute("categoryList",ss.selectCategoryList());
+    	
+    	
     	model.addAttribute("date",LocalDate.now());
     	model.addAttribute("now",LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM월-dd일-HH시 mm분 ")));
     	
