972ff73c32d9c15dd2c7d1f0ac3bd62e79be4951
Yoonjabin <jattange@gmail.com>
2026-02-20 15:40:20 +0900

    삭제 메세지

--- src/main/java/kr/co/sist/admin/product/AdminProductController.java
+++ src/main/java/kr/co/sist/admin/product/AdminProductController.java
@@ -80,27 +80,43 @@ public class AdminProductController {
 	@PostMapping("/manage/product/delete_product")
 	@ResponseBody 
 	public String deleteProduct(AdminProductDTO pDTO) {
+	    AdminProductDomain product = ps.getProductDetail(pDTO.getProduct_num());
+	    if (product == null) {
+	        return "fail";
+	    }
+
 	    int result = ps.removeProduct(pDTO); 
 	    
-	    AdminProductDomain product = ps.getProductDetail(pDTO.getProduct_num());
-    	ChatDTO cDTO = new ChatDTO();
-    	cDTO.setProductNum(product.getProduct_num());
-    	cDTO.setWriterId("SYSTEM");
-    	cDTO.setType("TEXT");
-    	//해당 상품의 판매자 아이디
-    	cDTO.setOtherId(product.getUser_id());
-    	
-    	StringBuilder msg = new StringBuilder();
-    	msg.append("상품 ").append(product.getTitle()).append("이 ").append(product.getDelete_text()).append("에 해당하여 삭제되었습니다.");
-    	cDTO.setContent(msg.toString());
-    	
-    	Integer roomNum = chatService.searchChatRoom(cDTO.getWriterId(), cDTO.getOtherId());
-    	cDTO.setRoomNum(roomNum);
-
-    	//실시간 메시지 전달
-    	chatService.sendMessage(cDTO);
-    	
-        messagingTemplate.convertAndSend("/topic/room/" + roomNum, cDTO);
+	    if (result > 0) {
+	        ChatDTO cDTO = new ChatDTO();
+	        cDTO.setProductNum(product.getProduct_num());
+	        cDTO.setWriterId("SYSTEM");
+	        cDTO.setType("TEXT");
+	        cDTO.setOtherId(product.getUser_id()); 
+	        String reasonStr = getDeleteReasonText(pDTO.getDelete_text());
+	        
+	        StringBuilder msg = new StringBuilder();
+	        msg.append("상품 [").append(product.getTitle()).append("]이 ")
+	           .append(reasonStr).append(" 사유로 삭제되었습니다.");
+	        cDTO.setContent(msg.toString());
+	        
+	        Integer roomNum = chatService.searchChatRoom(cDTO.getWriterId(), cDTO.getOtherId());
+	        cDTO.setRoomNum(roomNum);
+	        chatService.sendMessage(cDTO);
+	        messagingTemplate.convertAndSend("/topic/room/" + roomNum, cDTO);
+	    }
+	    
 	    return (result > 0) ? "success" : "fail";
+	}
+
+	private String getDeleteReasonText(int code) {
+	    return switch(code) {
+	        case 1 -> "사기로 의심됨";
+	        case 2 -> "부적절한 물품";
+	        case 3 -> "검색 노출 조작 및 도배";
+	        case 4 -> "현금 외 거래 유도";
+	        case 5 -> "기타 사유";
+	        default -> "운영정책 위반";
+	    };
 	}
 }
--- src/main/java/kr/co/sist/admin/product/AdminProductDTO.java
+++ src/main/java/kr/co/sist/admin/product/AdminProductDTO.java
@@ -1,49 +1,30 @@
 package kr.co.sist.admin.product;
 
-import java.sql.Date;
-
 import lombok.Getter;
-
 import lombok.Setter;
-
 import lombok.ToString;
 
 @Getter
-
 @Setter
-
 @ToString
-
 public class AdminProductDTO {
 
-	private int category_code;
+    private int category_code;
+    private int startNum;
+    private int endNum;
+    private int currentPage = 1;
+    private int totalPage;
+    private int pageScale = 15;
+    
+    private int product_num; 
+    private int delete_text; 
+    
+    private String keyword;
+    private String sortBy = "latest";
 
-	private int startNum;
-
-	private int endNum;
-
-	private int currentPage = 1;
-
-	private int totalPage;
-
-	private int pageScale = 15;
-
-	private int productNo;
-
-	private String deleteReason; // [필수] 매퍼의 #{deleteReason}과 이름이 똑같아야 함
-
-	private String keyword;
-
-	private String sortBy = "latest";
-
-	private int product_num;
-
-	public void setNumbers() {
-
-		this.startNum = (this.currentPage - 1) * this.pageScale + 1;
-
-		this.endNum = this.startNum + this.pageScale - 1;
-
-	}
+    public void setNumbers() {
+        this.startNum = (this.currentPage - 1) * this.pageScale + 1;
+        this.endNum = this.startNum + this.pageScale - 1;
+    }
 
 }
